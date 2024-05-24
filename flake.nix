8d08b99e54bde77e1d5d646a71bbc79edef23959
codgician <15964984+codgician@users.noreply.github.com>
2024-05-24 23:08:10 +0800

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A collection of slides made by codgician.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     reveal-js = {
       url = "github:hakimel/reveal.js";
